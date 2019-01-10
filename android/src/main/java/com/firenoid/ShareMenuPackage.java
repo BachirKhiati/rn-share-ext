eebe84d2bcec661b1280c8e15647f9bf6312d5cc
Bachir Khiati <bachir.khiati@gmail.com>
2019-01-10 11:00:10 +0200

    quick update

--- android/src/main/java/com/firenoid/ShareMenuPackage.java
+++ android/src/main/java/com/firenoid/ShareMenuPackage.java
@@ -23,10 +23,10 @@ public class ShareMenuPackage implements ReactPackage {
     modules.add(new ShareMenuModule(reactContext));
     return modules;
   }
-  
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-     return Collections.emptyList();
-  }
+
+   public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
